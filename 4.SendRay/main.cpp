ccb0f7d604344be73a53a123ff687e5df85337c3
neptunehjf <neptunehjf@sina.com>
2024-12-16 13:29:49 +0900

    4.SendRay 构造viewport，并计算出camera到viewport的个像素的ray

--- 4.SendRay/main.cpp
+++ 4.SendRay/main.cpp
@@ -1,10 +1,35 @@
 #include "vec3.h"
 #include "color.h"
+#include "ray.h"
+
+color ray_color(const ray& r);
 
 int main() {
 
-    int image_width = 256;
-    int image_height = 256;
+    // Image 固定宽高比为16 : 9 
+    double aspect_radio = 16.0 / 9.0;
+    int image_width = 400;
+    int image_height = (int) (image_width / aspect_radio); // 因为是近似计算，有可能是0
+    image_height = (image_height < 1) ? 1 : image_height;  // 保证不小于1
+
+    // Camera
+    vec3 cam_pos = { 0.0, 0.0, 0.0 };
+    double focal_length = 1.0; // 焦距，camera和viewport之间的距离，目前定为1.0
+    double viewport_h = 2.0;
+    double viewport_w = viewport_h * (double(image_width) / image_height); // 因为image_height是近似计算的，不能直接用aspect_radio
+
+    // viewport UV 在camera坐标系的向量
+    vec3 u = vec3(viewport_w, 0.0, 0.0);
+    vec3 v = vec3(0.0, -viewport_h, 0.0); // 因为viewport的所在的v方向和camera坐标系y方向相反
+
+    // 换算成每个pixel对应的uv大小
+    double pixel_x = viewport_w / image_width;
+    double pixel_y = -viewport_h / image_height;
+
+    // 计算viewport原点在camera坐标系的位置，viewport原点在左上角，离相机距离focal_length，在xy平面，相机位置和viewport中心重合
+    point3 viewport_zero = cam_pos - vec3(0.0, 0.0, focal_length) - u / 2 - v / 2;
+    // 转为像素的中心坐标
+    point3 pixel_zero = viewport_zero + vec3(0.5 * pixel_x, 0.5 * pixel_y, 0.0);
 
     cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
@@ -16,12 +41,29 @@ int main() {
 
         for (int i = 0; i < image_width; i++) // R
         {
-            double r = (double)i / (double)(image_width - 1);
-            double g = (double)j / (double)(image_height - 1);
-            double b = 0.0;
+            // 求出每一个camera到像素中心的射线
+            point3 pixel = pixel_zero + vec3(i * pixel_x, j * pixel_y, 0.0);
+            vec3 ray_dir = pixel - cam_pos;
+            ray r(cam_pos, ray_dir);
 
-            write_color(cout, vec3(r, g, b));
+            // 根据ray算出color
+            color pixel_color = ray_color(r);
+            write_color(cout, pixel_color);
         }
     }
     clog << "\rDone.                 \n";
+}
+
+// 计算光线的颜色
+color ray_color(const ray& r)
+{
+    // 根据r的y分量进行lerp
+    vec3 r_uint = unit_vector(r.direction());
+    // [-1.0, 1.0] ==> [0.0, 1.0]
+    double a = 0.5 * (r_uint.y() + 1.0);
+
+    // 蓝色与白色之间lerp
+    color c = (1 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
+
+    return c;
 }
