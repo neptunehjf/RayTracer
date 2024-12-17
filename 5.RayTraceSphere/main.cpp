9bd60c7d5a11e148172af6ab53c943c2f82e94b5
neptunehjf <neptunehjf@sina.com>
2024-12-17 15:43:04 +0900

    5.RayTraceSphere ray和sphere求交算法
    对于中心在(0,0,-1)，半径为0.5的sphere，和ray求交，如果有交点，则ray颜色返回红色

--- 5.RayTraceSphere/main.cpp
+++ 5.RayTraceSphere/main.cpp
@@ -3,6 +3,7 @@
 #include "ray.h"
 
 color ray_color(const ray& r);
+bool is_sphere_hit(const point3& center, double radius, const ray& r);
 
 int main() {
 
@@ -57,13 +58,39 @@ int main() {
 // 计算光线的颜色
 color ray_color(const ray& r)
 {
+    // 如果光线与球体相交，则光线为红色
+    if (is_sphere_hit(point3(0.0, 0.0, -1.0), 0.5, r))
+        return color(1.0, 0.0, 0.0);
+
+    // 以下为背景色
     // 根据r的y分量进行lerp
     vec3 r_uint = unit_vector(r.direction());
     // [-1.0, 1.0] ==> [0.0, 1.0]
     double a = 0.5 * (r_uint.y() + 1.0);
 
-    // 蓝色与白色之间lerp
-    color c = (1 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
+    // 返回蓝色与白色之间的lerp
+    return (1 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);;
+}
 
-    return c;
+// 判断ray是否和球相交
+bool is_sphere_hit(const point3& center, double radius, const ray& r)
+{
+    point3 o = r.origin();
+    vec3 d = r.direction();
+    vec3 oc = center - o;
+
+    // 转化为二项式求解未知数t的问题，推导过程参考derivation/sphere_hit.jpg
+    double a = dot(d, d);
+    double b = -2 * dot(d, oc);
+    double c = dot(oc, oc) - radius * radius;
+
+    double discriminant = b * b - 4 * a * c;
+
+    // 如果判别式等于0，则ray和sphere只有一个交点
+    // 如果判别式大于0，则ray和sphere有两个交点
+    // 如果判别式小于0，则ray和sphere有没有交点
+    if (discriminant >= 0)
+        return true;
+
+    return false;
 }
