62a9dcef9e87f40de39de1936e8eeb6157d8ce20
neptunehjf <neptunehjf@sina.com>
2025-01-03 11:27:57 +0900

    13.Defocus Blur 新建工程

---
+++ 13.Defocus Blur/common.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <iostream>
+#include <limits>
+#include <cmath>
+#include <vector>
+#include <memory>
+#include <random>
+
+// C++ Std Usings
+using namespace std;
+
+// Constants
+const double inf = numeric_limits<double>::infinity();
+const double pi = 3.1415926535897932385;
+
+// Utility Functions
+inline double degree_to_radian(double degree)
+{
+	return degree * pi / 180.0;
+}
+
+inline double random_double()
+{
+	// 这里全局只需1个种子，1个伪随机数列，因此加static修饰       
+	static uniform_real_distribution<double> distribution(0.0, 1.0);
+	static mt19937 generator;
+	return distribution(generator);
+}
+
+inline double random_double(double min, double max)
+{
+	// 这里全局只需1个种子，1个伪随机数列，因此加static修饰       
+	static uniform_real_distribution<double> distribution(min, max);
+	static mt19937 generator;
+	return distribution(generator);
+}
+
+// Common Headers
+#include "vec3.h"
+#include "color.h"
+#include "ray.h"
