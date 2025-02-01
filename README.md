e055740c1ef367e99589fccf94fbf925fe235acf
neptunehjf <neptunehjf@sina.com>
2025-02-02 08:11:09 +0900

    x6.Quadrilaterals new project

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # RayTracer
 
 Configuration Properties⇒Debugging⇒Command Arguments                         >>output.ppm
-Configuration Properties⇒VC++ Directories⇒Executable Directories 　          ..\Dependency\Includes\stb
+Configuration Properties⇒VC++ Directories⇒Include Directories 　          ..\Dependency\Includes\stb
 Configuration Properties⇒C/C++⇒Preprocessor⇒Preprocessor Definitions    _CRT_SECURE_NO_WARNINGS
 
---
+++ x6.Quadrilaterals/interval.h
@@ -0,0 +1,48 @@
+#pragma once
+#include "common.h"
+
+class interval
+{
+public:
+	double min, max;
+
+	interval() : min(inf), max(-inf) {}
+	interval(double min, double max) : min(min), max(max) {}
+
+	interval(const interval& a, const interval& b)
+	{
+		min = std::min(a.min, b.min);
+		max = std::max(a.max, b.max);
+	}
+
+	double size() const { return max - min; }
+
+	double contains(double x) const { return (min <= x && x <= max); }
+
+	double surrounds(double x) const { return (min < x && x < max); }
+
+	double clamp(double x) const
+	{
+		if (x < min)
+			return min;
+		else if (x > max)
+			return max;
+		else
+			return x;
+	}
+
+	// ray与slab重合的场合，t_min和t_max都是NaN，所以(t_min <= t_max)的结果必为false
+	// 但是实际上这种情况也应该算是有交点(grazing angle)，所以稍微扩充一下AABB的大小，使(t_min <= t_max)的结果为true
+	// 具体参考 referrence/aabb.jpg
+	interval expand(double delta) const
+	{
+		double padding = delta / 2;
+		return interval(min - padding, max + padding);
+	}
+
+	// 声明和类相同类型的静态变量是合法的，不会导致递归定义
+	static const interval empty, universe;
+};
+
+const interval interval::empty = interval(inf, -inf);
+const interval interval::universe = interval(-inf, inf);
